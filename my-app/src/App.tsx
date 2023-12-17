8bc9525b2fac39e522579f25b2eff454645001c9
HelloSeaNation <sookann@gmail.com>
2023-12-18 12:01:50 +1300

    Main page and top menu

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@chakra-ui/react";
+import { Hash } from "crypto";
+import TopMenu from "./components/TopMenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <HashRouter basename="/">
+      <Box backgroundColor="#F5F5F5" minHeight={"100vh"}>
+        <Box
+          backgroundColor="#E8BCBC"
+          minHeight={"5vh"}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box m={[15]}>
+            <img src="../aurora.svg" height={"30vh"} />
+          </Box>
+        </Box>
+        <TopMenu />
+        <Box>
+          <Routes>
+            <Route path="/home"/>
+            <Route path="/about" />
+            <Route path="/contact" />
+          </Routes>
+        </Box>
+      </Box>
+      </HashRouter>
+    </>
   );
 }
 
