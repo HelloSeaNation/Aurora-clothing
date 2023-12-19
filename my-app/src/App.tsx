8b074c09c984464329afd37a9b7f056fb33bc6db
Keshia-w <keshia.w196@gmail.com>
2023-12-19 13:11:04 +1300

    top menu items added and some styling

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import { Box } from "@chakra-ui/react";
-import { Hash } from "crypto";
 import TopMenu from "./components/TopMenu";
 
 function App() {
--- my-app/src/components/TopMenu.tsx
+++ my-app/src/components/TopMenu.tsx
@@ -1,8 +1,25 @@
+import { Box } from "@chakra-ui/react";
+
+const categoryStyles = 
+{display: "grid", 
+gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
+fontSize: "9px",
+paddingLeft: "325px",
+paddingRight: "275px",
+}; 
+
+const textStyles = {fontFamily: "Koulen"};
+
 function TopMenu() {
     return (
-        <div>
-            <h1>Top Menu</h1>
-        </div>
+        <Box style={categoryStyles}>
+            <h1>New</h1>
+            <h1>Clothing</h1>
+            <h1>Accessories</h1>
+            <h1>Swim</h1>
+            <h1>Sale</h1>
+            <h1>Contact us</h1>
+        </Box>
     );
 }
 export default TopMenu;
