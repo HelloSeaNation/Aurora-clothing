eed4b5227a925762470a8c9d3db8adf7d9f2d6ca
HelloSeaNation <sookann@gmail.com>
2023-12-21 08:16:20 +1300

    Font change and center the text

--- my-app/src/components/TopMenu.tsx
+++ my-app/src/components/TopMenu.tsx
@@ -1,48 +1,47 @@
-import 
-{ Box,
-Menu,
-MenuButton,
-MenuList,
-MenuItem} from "@chakra-ui/react";
-import {Link} from "react-router-dom"
+import {
+  Box,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Flex,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import React, { ReactNode } from "react";
 
-const categoryStyles = 
-{display: "grid", 
-gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
-fontSize: "9px",
-paddingLeft: "325px",
-paddingRight: "275px",
-}; 
+const categoryStyles = {
+  display: "grid",
+  gridTemplateColumns: "repeat(6, 1fr)",
+  fontSize: "20px",
+  fontFamily: "Koulen",
+  justifyItems: "center",
+};
 
 const hoverStyles = {
-    cursor: "pointer"
-    };
-  
-    // Define the type for the children prop
-  interface CategoryItemProps {
-    children: ReactNode;
-  }
-  
-  // Individual category item component with hover effect
-  const CategoryItem: React.FC<CategoryItemProps> = ({ children }) => (
-    <Box _hover={hoverStyles}>
-      <h1>{children}</h1>
-    </Box>
-  );
+  cursor: "pointer",
+};
 
-  
-  
-  const TopMenu = () => (
-    <Box style={categoryStyles}>
-      <CategoryItem>New</CategoryItem>
-      <CategoryItem>Clothing</CategoryItem>
-      <CategoryItem>Accessories</CategoryItem>
-      <CategoryItem>Swim</CategoryItem>
-      <CategoryItem>Sale</CategoryItem>
-      <Link to="./faqs">
-        <CategoryItem>Contact us</CategoryItem>
-      </Link>
-    </Box>
-  );
+// Define the type for the children prop
+interface CategoryItemProps {
+  children: ReactNode;
+}
+
+// Individual category item component with hover effect
+const CategoryItem: React.FC<CategoryItemProps> = ({ children }) => (
+    <Text _hover={hoverStyles}>{children}</Text>
+);
+
+const TopMenu = () => (
+  <Box style={categoryStyles}>
+    <CategoryItem>New</CategoryItem>
+    <CategoryItem>Clothing</CategoryItem>
+    <CategoryItem>Accessories</CategoryItem>
+    <CategoryItem>Swim</CategoryItem>
+    <CategoryItem>Sale</CategoryItem>
+    <Link to="./faqs">
+      <CategoryItem>Contact us</CategoryItem>
+    </Link>
+  </Box>
+);
 export default TopMenu;
