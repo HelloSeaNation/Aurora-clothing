2f306e5d30729347283636be173f8dd1da02f1d2
HelloSeaNation <sookann@gmail.com>
2023-12-21 10:12:22 +1300

    Topbar position and add icon

--- my-app/src/components/TopBar.tsx
+++ my-app/src/components/TopBar.tsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { Box, Center, Flex } from "@chakra-ui/react";
+import { Box, Center, Flex, Image } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
 
 function TopBar() {
   return (
-    <>
-      <Flex
-        backgroundColor="#E8BCBC"
-        minHeight={"5vh"}
-        maxWidth={"100%"}
-      >
-        <Box m={[15]} flex="5">
-          <img src="../aurora.svg" height={"30vh"} />
-        </Box>
-        <Box flex="1" style={{ fontSize: "20px" }}>
-          <SearchIcon color="red" />
-        </Box>
-      </Flex>
-    </>
+    <Flex
+      backgroundColor="#E8BCBC"
+      minHeight={"5vh"}
+      maxWidth={"100%"}
+      justifyContent="space-between" // To space the elements
+      alignItems="center" // To vertically center the items
+      paddingLeft="80px" // Add padding for some space around elements
+    >
+      <Box flex="1" style={{ textAlign: "center" }}>
+        <Image src="../aurora.svg" height={"3vh"} margin={"20px"} />
+      </Box>
+      <Box style={{ fontSize: "30px" }} paddingRight="20px">
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          color="#F5F5F5"
+          style={{ paddingRight: "20px" }}
+        />
+        <FontAwesomeIcon icon={faCartShopping} color="#F5F5F5" />
+      </Box>
+    </Flex>
   );
 }
 
