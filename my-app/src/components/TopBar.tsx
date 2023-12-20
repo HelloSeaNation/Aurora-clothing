576e5b7d97fc8032ab6031153227144f6c186619
Keshia-w <keshia.w196@gmail.com>
2023-12-21 11:08:11 +1300

    Aurora topbar icon routed to landing page

--- my-app/src/components/TopBar.tsx
+++ my-app/src/components/TopBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Center, Flex, Image } from "@chakra-ui/react";
+import {Link} from 'react-router-dom'
 import { SearchIcon } from "@chakra-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -18,7 +19,9 @@ function TopBar() {
       paddingLeft="15vh" // Add padding for some space around elements
     >
       <Box flex="1" style={{ textAlign: "center" }}>
-        <Image src="../aurora.svg" height={"3vh"} margin={"20px"} />
+        <Link to="/">
+          <Image src="../aurora.svg" height={"3vh"} margin={"20px"} />
+        </Link>
       </Box>
       <Box style={{ fontSize: "30px" }} paddingRight="40px">
         <FontAwesomeIcon
