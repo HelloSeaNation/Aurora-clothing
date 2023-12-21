08ebf35e42c084981c961b35c8e8efa8641e403e
Keshia-w <keshia.w196@gmail.com>
2023-12-22 10:46:37 +1300

    contact us page form created, maps added

--- my-app/src/pages/contact-us.tsx
+++ my-app/src/pages/contact-us.tsx
@@ -1,11 +1,65 @@
-import {Box} from '@chakra-ui/react'
+import {
+    Box,
+    Input,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    AccordionIcon
+} from '@chakra-ui/react'
 import Map from '../components/Map'
 
 export const ContactUs = () =>{
+
+    const textStyles = {
+        fontFamily: "Koulen"
+    }
+
+    const inputStyle = {
+        width: '100%',
+        height: '35px', // Adjust the height as needed
+        marginBottom: 0,
+    };
+
+
     return(
-        <div>
-            {/* <Map /> */}
-        </div>
+        <Box style={textStyles}>
+            <Box textAlign="center">
+                <h1>Contact Us</h1>
+            </Box>
+
+            <Box textAlign="center" style={{ width: 500, margin: '0 auto' }}>
+                <Box as="h3" style={{ textAlign: 'left', marginBottom: 0 }}>Name</Box>
+                <Input style={inputStyle}></Input>
+
+                <Box as="h3" style={{ textAlign: 'left', marginBottom: 0 }}>Email</Box>
+                <Input style={inputStyle}></Input>
+
+                <Box as="h3" style={{ textAlign: 'left', marginBottom: 0 }}>Question type</Box>
+                <Menu>
+                    <MenuButton as="button" style={inputStyle}>
+                        <Box></Box>
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>My order never arrived</MenuItem>
+                        <MenuItem>Problem with customer service</MenuItem>
+                        <MenuItem>Issue with product quality</MenuItem>
+                        <MenuItem>Request refund/exchange</MenuItem>
+                    </MenuList>
+                </Menu>
+
+                <Box as="h3" style={{ textAlign: 'left', marginBottom: 0 }}>Write your question</Box>
+                <Input style={{...inputStyle, height: "125px"}}></Input>
+            </Box>
+
+            <Box textAlign="center">
+                <h1>Or</h1>
+            </Box>
+
+            <Box>
+                <Map width="50%" height="450"/>
+            </Box>
+        </Box>
     )
 }
 
