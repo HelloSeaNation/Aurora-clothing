3ea6b455dbd2f669df0fb42811a3220c190fa5b0
Keshia-w <keshia.w196@gmail.com>
2023-12-22 11:34:04 +1300

    contact page styling

--- my-app/src/pages/contact-us.tsx
+++ my-app/src/pages/contact-us.tsx
@@ -7,6 +7,7 @@ import {
     MenuItem,
     AccordionIcon
 } from '@chakra-ui/react'
+import React, {useState} from 'react'
 import Map from '../components/Map'
 
 export const ContactUs = () =>{
@@ -17,10 +18,17 @@ export const ContactUs = () =>{
 
     const inputStyle = {
         width: '100%',
-        height: '35px', // Adjust the height as needed
+        height: '35px',
         marginBottom: 0,
+        border:"1px solid #D1D1D1",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+        verticalAlign: "top"
     };
 
+    const [selectedOption, setSelectedOption] = useState('');
+    const handleMenuClick = (value: string) => {
+        setSelectedOption(value);
+    }
 
     return(
         <Box style={textStyles}>
@@ -37,27 +45,47 @@ export const ContactUs = () =>{
 
                 <Box as="h3" style={{ textAlign: 'left', marginBottom: 0 }}>Question type</Box>
                 <Menu>
-                    <MenuButton as="button" style={inputStyle}>
-                        <Box></Box>
+                    <MenuButton as="button" style={{...inputStyle, backgroundColor: "white", textAlign: 'left'}}>
+                        <Box>{selectedOption}</Box>
                     </MenuButton>
+                    
                     <MenuList>
-                        <MenuItem>My order never arrived</MenuItem>
-                        <MenuItem>Problem with customer service</MenuItem>
-                        <MenuItem>Issue with product quality</MenuItem>
-                        <MenuItem>Request refund/exchange</MenuItem>
+                        <MenuItem onClick={() => handleMenuClick('My order never arrived')}>My order never arrived</MenuItem>
+                        <MenuItem onClick={() => handleMenuClick('Problem with customer service')}>Problem with customer service</MenuItem>
+                        <MenuItem onClick={() => handleMenuClick('Issue with product quality')}>Issue with product quality</MenuItem>
+                        <MenuItem onClick={() => handleMenuClick('Request refund/exchange')}>Request refund/exchange</MenuItem>
+                        <MenuItem onClick={() => handleMenuClick('Other')}>Other</MenuItem>
                     </MenuList>
+                    
                 </Menu>
 
                 <Box as="h3" style={{ textAlign: 'left', marginBottom: 0 }}>Write your question</Box>
                 <Input style={{...inputStyle, height: "125px"}}></Input>
+
+                <Box 
+                as="button" 
+                style={{backgroundColor: "#E8BCBC", 
+                color: "white", 
+                border: "0", 
+                padding: "10px 30px", 
+                fontSize: "20px",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                borderRadius: "10px",
+                fontFamily: "Koulen",
+                marginTop: "20px"}}>Send</Box>
             </Box>
 
             <Box textAlign="center">
                 <h1>Or</h1>
             </Box>
 
-            <Box>
+            <Box display="flex" padding="10px" align-items="center" justify-content="space-between" margin="0 50px">
                 <Map width="50%" height="450"/>
+                <Box display="grid" margin="115px">
+                    <Box as="h3">Phone: +64 27 783 7824</Box>
+                    <Box as="h3">E-mail: aurora@gmail.com</Box>
+                    <Box as="h3">128 Riccarton Road, Riccarton, Christchurch 8041</Box>
+                </Box>
             </Box>
         </Box>
     )
