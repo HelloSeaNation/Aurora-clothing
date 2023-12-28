2dee8ac83f5f450c5e651cb757723702c34a1a90
HelloSeaNation <sookann@gmail.com>
2023-12-28 18:34:39 +1300

    Dress image done

--- my-app/src/pages/DressesPage.tsx
+++ my-app/src/pages/DressesPage.tsx
@@ -4,9 +4,9 @@ import {
   Card,
   Image,
   CardBody,
-  Grid,
-  GridItem,
   SimpleGrid,
+  Flex,
+  Box,
 } from "@chakra-ui/react";
 import { text } from "stream/consumers";
 
@@ -17,9 +17,20 @@ const textStyles = {
 
 const cardStyles = {
   fontFamily: "Koulen",
-  margin: "auto",
   fontSize: "20px",
-  fontWeight: "600",
+  fontWeight: "500",
+  width: "30vh",
+};
+
+const cardImage = {
+  width: "21vh",
+  height: "auto",
+};
+
+const boxStyle ={
+    flexGrow: 1,
+    marginBottom: "-50px",
+    marginTop: "-25px",
 };
 
 function Dresses() {
@@ -36,119 +47,148 @@ function Dresses() {
       </Text>
       <SimpleGrid spacingY={60} columns={4} width={"85%"} margin={"auto"}>
         <Card style={cardStyles}>
-          <CardBody>
-            <Image
-              src="./assets/baby-blue-floral-front.png"
-              width={"18vh"}
-              height={"auto"}
-            />
-            <Grid
-              templateRows="repeat(2, 1fr)"
-              templateColumns="repeat(2, 1fr)"
-            >
-              <GridItem colSpan={2} rowSpan={1}>
-                <Text style={cardStyles}>Baby Blue Floral Dress</Text>
-              </GridItem>
-              <GridItem colSpan={1} rowSpan={1}>
-                <Text style={cardStyles}>$49/99</Text>
-              </GridItem>
-              <GridItem colSpan={1}>
-                <Text style={cardStyles}>Fivestar</Text>
-              </GridItem>
-            </Grid>
-          </CardBody>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image
+                src="./assets/baby-blue-floral-front.png"
+                style={cardImage}
+              />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Baby Blue Floral Dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$49.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
         </Card>
         <Card style={cardStyles}>
-          <CardBody>
-            <Image
-              src="./assets/black-short-sleeve-front.png"
-              width={"18vh"}
-              height={"auto"}
-            />
-            <Grid
-              templateRows="repeat(2, 1fr)"
-              templateColumns="repeat(2, 1fr)"
-            >
-              <GridItem colSpan={2} rowSpan={1}>
-                <Text style={cardStyles}>Black short sleeve</Text>
-              </GridItem>
-              <GridItem colSpan={1} rowSpan={1}>
-                <Text style={cardStyles}>$49/99</Text>
-              </GridItem>
-              <GridItem colSpan={1}>
-                <Text style={cardStyles}>Fivestar</Text>
-              </GridItem>
-            </Grid>
-          </CardBody>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image
+                src="./assets/black-short-sleeve-front.png"
+                style={cardImage}
+              />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Black short sleeve</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$25.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
         </Card>
         <Card style={cardStyles}>
-          <CardBody>
-            <Image
-              src="./assets/dark-blue-short-front.png"
-              width={"18vh"}
-              height={"auto"}
-            />
-            <Grid
-              templateRows="repeat(2, 1fr)"
-              templateColumns="repeat(2, 1fr)"
-            >
-              <GridItem colSpan={2} rowSpan={1}>
-                <Text style={cardStyles}>Dark Blue short</Text>
-              </GridItem>
-              <GridItem colSpan={1} rowSpan={1}>
-                <Text style={cardStyles}>$49/99</Text>
-              </GridItem>
-              <GridItem colSpan={1}>
-                <Text style={cardStyles}>Fivestar</Text>
-              </GridItem>
-            </Grid>
-          </CardBody>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image
+                src="./assets/dark-blue-short-front.png"
+                style={cardImage}
+              />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>dark blue short dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$48.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
         </Card>
         <Card style={cardStyles}>
-          <CardBody>
-            <Image
-              src="./assets/green-strap-front.png"
-              width={"18vh"}
-              height={"auto"}
-            />
-            <Grid
-              templateRows="repeat(2, 1fr)"
-              templateColumns="repeat(2, 1fr)"
-            >
-              <GridItem colSpan={2} rowSpan={1}>
-                <Text style={cardStyles}>Green Strap dress</Text>
-              </GridItem>
-              <GridItem colSpan={1} rowSpan={1}>
-                <Text style={cardStyles}>$49/99</Text>
-              </GridItem>
-              <GridItem colSpan={1}>
-                <Text style={cardStyles}>Fivestar</Text>
-              </GridItem>
-            </Grid>
-          </CardBody>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image src="./assets/green-strap-front.png" style={cardImage} />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Green Strap dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$39.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
         </Card>
         <Card style={cardStyles}>
-          <CardBody>
-            <Image
-              src="./assets/leopard-dress-front.png"
-              width={"18vh"}
-              height={"auto"}
-            />
-            <Grid
-              templateRows="repeat(2, 1fr)"
-              templateColumns="repeat(2, 1fr)"
-            >
-              <GridItem colSpan={2} rowSpan={1}>
-                <Text style={cardStyles}>Leopard pattern dress</Text>
-              </GridItem>
-              <GridItem colSpan={1} rowSpan={1}>
-                <Text style={cardStyles}>$49/99</Text>
-              </GridItem>
-              <GridItem colSpan={1}>
-                <Text style={cardStyles}>Fivestar</Text>
-              </GridItem>
-            </Grid>
-          </CardBody>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image src="./assets/leopard-dress-front.png" style={cardImage} />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>leopard pattern dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$55.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
+        </Card>
+        <Card style={cardStyles}>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image
+                src="./assets/light-brown-floral-front.png"
+                style={cardImage}
+              />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Light brown floral dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$39.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
+        </Card>
+        <Card style={cardStyles}>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image
+                src="./assets/long-blue-floral-front.png"
+                style={cardImage}
+              />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Long Blue floral dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$49/99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
+        </Card>
+        <Card style={cardStyles}>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image src="./assets/orange-dress-front.png" style={cardImage} />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Orange dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$43.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
+        </Card>
+        <Card style={cardStyles}>
+          <Box style={{ display: "flex", flexDirection: "column" }}>
+            <CardBody>
+              <Image
+                src="./assets/red-stripe-dress-front.png"
+                style={cardImage}
+              />
+            </CardBody>
+            <Box style={boxStyle}>
+              <Text style={cardStyles}>Red-stripe dress</Text>
+            </Box>
+            <Flex justifyContent="space-between">
+              <Text style={cardStyles}>$54.99</Text>
+              <Text style={cardStyles}>Fivestar</Text>
+            </Flex>
+          </Box>
         </Card>
       </SimpleGrid>
     </>
