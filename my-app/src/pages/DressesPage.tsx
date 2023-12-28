6cc532b6fde81a9af2a64ead2f155172fbe8d08a
HelloSeaNation <sookann@gmail.com>
2023-12-28 13:50:58 +1300

    image location change and dress page (half way )

--- my-app/src/pages/DressesPage.tsx
+++ my-app/src/pages/DressesPage.tsx
@@ -1,9 +1,158 @@
-import React from 'react'
+import React from "react";
+import {
+  Text,
+  Card,
+  Image,
+  CardBody,
+  Grid,
+  GridItem,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { text } from "stream/consumers";
 
-export const Dresses = () => {
-    return(
-        <div></div>
-    )
+const textStyles = {
+  fontFamily: "Koulen",
+  margin: "auto",
+};
+
+const cardStyles = {
+  fontFamily: "Koulen",
+  margin: "auto",
+  fontSize: "20px",
+  fontWeight: "600",
+};
+
+function Dresses() {
+  return (
+    <>
+      <Text
+        fontWeight={"600"}
+        fontSize={"30px"}
+        padding={"20px 0 20px 0"}
+        width={"80%"}
+        style={textStyles}
+      >
+        Dresses
+      </Text>
+      <SimpleGrid spacingY={60} columns={4} width={"85%"} margin={"auto"}>
+        <Card style={cardStyles}>
+          <CardBody>
+            <Image
+              src="./assets/baby-blue-floral-front.png"
+              width={"18vh"}
+              height={"auto"}
+            />
+            <Grid
+              templateRows="repeat(2, 1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem colSpan={2} rowSpan={1}>
+                <Text style={cardStyles}>Baby Blue Floral Dress</Text>
+              </GridItem>
+              <GridItem colSpan={1} rowSpan={1}>
+                <Text style={cardStyles}>$49/99</Text>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <Text style={cardStyles}>Fivestar</Text>
+              </GridItem>
+            </Grid>
+          </CardBody>
+        </Card>
+        <Card style={cardStyles}>
+          <CardBody>
+            <Image
+              src="./assets/black-short-sleeve-front.png"
+              width={"18vh"}
+              height={"auto"}
+            />
+            <Grid
+              templateRows="repeat(2, 1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem colSpan={2} rowSpan={1}>
+                <Text style={cardStyles}>Black short sleeve</Text>
+              </GridItem>
+              <GridItem colSpan={1} rowSpan={1}>
+                <Text style={cardStyles}>$49/99</Text>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <Text style={cardStyles}>Fivestar</Text>
+              </GridItem>
+            </Grid>
+          </CardBody>
+        </Card>
+        <Card style={cardStyles}>
+          <CardBody>
+            <Image
+              src="./assets/dark-blue-short-front.png"
+              width={"18vh"}
+              height={"auto"}
+            />
+            <Grid
+              templateRows="repeat(2, 1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem colSpan={2} rowSpan={1}>
+                <Text style={cardStyles}>Dark Blue short</Text>
+              </GridItem>
+              <GridItem colSpan={1} rowSpan={1}>
+                <Text style={cardStyles}>$49/99</Text>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <Text style={cardStyles}>Fivestar</Text>
+              </GridItem>
+            </Grid>
+          </CardBody>
+        </Card>
+        <Card style={cardStyles}>
+          <CardBody>
+            <Image
+              src="./assets/green-strap-front.png"
+              width={"18vh"}
+              height={"auto"}
+            />
+            <Grid
+              templateRows="repeat(2, 1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem colSpan={2} rowSpan={1}>
+                <Text style={cardStyles}>Green Strap dress</Text>
+              </GridItem>
+              <GridItem colSpan={1} rowSpan={1}>
+                <Text style={cardStyles}>$49/99</Text>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <Text style={cardStyles}>Fivestar</Text>
+              </GridItem>
+            </Grid>
+          </CardBody>
+        </Card>
+        <Card style={cardStyles}>
+          <CardBody>
+            <Image
+              src="./assets/leopard-dress-front.png"
+              width={"18vh"}
+              height={"auto"}
+            />
+            <Grid
+              templateRows="repeat(2, 1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem colSpan={2} rowSpan={1}>
+                <Text style={cardStyles}>Leopard pattern dress</Text>
+              </GridItem>
+              <GridItem colSpan={1} rowSpan={1}>
+                <Text style={cardStyles}>$49/99</Text>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <Text style={cardStyles}>Fivestar</Text>
+              </GridItem>
+            </Grid>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
+    </>
+  );
 }
 
 export default Dresses;
