61f4aa6394d9ff893bdd140e14bd182aef76e406
HelloSeaNation <sookann@gmail.com>
2023-12-30 21:04:50 +1300

    Carousel merge

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,20 +1,24 @@
 import { Image, Box, Text } from "@chakra-ui/react";
+import NewReleaseCarousel from "../components/NewReleaseCarousel";
+import dressData from "../hooks/dressdata.json";
+import pantsData from "../hooks/pants-data.json";
 
 export const Home = () => {
+  const combinedData = [...dressData, ...pantsData];
+
   return (
     <Box h={"100vh"}>
       <Image src="../comfortpic.png" width={"100%"}></Image>
-
       <Text
         fontFamily={"Khula"}
         fontWeight={"600"}
         fontSize={"20px"}
         margin={"30px"}
       >
-        New Releases
+        New Release
       </Text>
+      <NewReleaseCarousel clothingData={combinedData} />;
     </Box>
-    
   );
 };
 
