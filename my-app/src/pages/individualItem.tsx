c61e1293a326e1c0af79b534ef23dd4b61f342a3
Keshia-w <115118959+Keshia-w@users.noreply.github.com>
2023-12-30 14:48:10 +1300

    dress data to individual item page working. Need pant data to work

--- my-app/src/pages/individualItem.tsx
+++ my-app/src/pages/individualItem.tsx
@@ -1,24 +1,51 @@
 import dresses from "../hooks/dressdata.json";
+import pant from "../hooks/pants-data.json";
 import React from "react";
 import { useParams } from "react-router-dom";
 
-interface RouteParams {
-  itemId: string;
+interface Item {
+  id: number; 
+  name: string;
+  price: number;
+  imgUrl: string;
+  hoverImage: string;
 }
+
+const findItemById = (itemId: string): Item | undefined => {
+  const dressItem = dresses.find((item) => item.id.toString() === itemId);
+  const pantItem = pant.find((item) => item.id.toString() === itemId);
+
+  if (dressItem) {
+    return dressItem;
+  } else if (pantItem) {
+    return pantItem;
+  }
+
+  return undefined;
+};
 
 const IndividualItem: React.FC = () => {
   const { itemId } = useParams() as { itemId: string };
 
   if (!itemId) {
     // Error handle
-    return <div>Item ID not found</div>;
+    return <div>Item not available</div>;
   }
 
+  const item = findItemById(itemId);
+
+  if (!item) {
+    // Handle item not found
+    return <div>Item not found</div>;
+  }
+
+  const { name, price, imgUrl } = item;
 
   return (
     <div>
-      <h1>Item ID: {itemId}</h1>
-      {/* Add other details as needed */}
+      <h1>Name: {name}</h1>
+      <h1>Price: ${price}</h1>
+      <img src={imgUrl} />
     </div>
   );
 };
